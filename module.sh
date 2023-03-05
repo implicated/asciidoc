e3c2ba8c082f7929189cbbe5597bf5ca4fdbdfb2
Implicated <implicated.cc@gmail.com>
2023-03-05 15:54:40 +0800

    test

---
+++ module.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+dir_path='docs/'
+mkdir -p ${dir_path}'attachments' ${dir_path}'examples' ${dir_path}'images' ${dir_path}'pages' ${dir_path}'partials'
+touch ${dir_path}'nav.adoc'
+touch ${dir_path}'pages/index.adoc'
